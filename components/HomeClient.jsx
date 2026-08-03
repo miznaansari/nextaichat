e7489fa8617600d0b81f718c6db4bf03bfcdfb02
A2D-mohd-mizna-ansari <mohd.ansari@a2deats.com>
2026-08-03 11:53:06 +0530

    fixed

--- components/HomeClient.jsx
+++ components/HomeClient.jsx
@@ -224,7 +224,7 @@ export default function HomeClient({ blogs, samplePersonas, appUrl }) {
           </a>
         </div>
 
-        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 gap-5">
+        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-5">
           {samplePersonas.map((persona, idx) => (
             <div
               key={idx}
